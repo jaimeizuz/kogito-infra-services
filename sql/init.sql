13f6fa3bb354e29d264070b8017babb0db2dfd70
Jaime Izuzquiza Suárez-Inclán <jizuzquiza@hotmail.es>
2025-04-03 21:07:42 +0200

    XA Transactions persisted in DB

--- sql/init.sql
+++ sql/init.sql
@@ -16,6 +16,15 @@ CREATE DATABASE kogito
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
+CREATE DATABASE xa-object-store
+    WITH
+    OWNER = "kogito-user"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 CREATE DATABASE keycloak
     WITH
     OWNER = "kogito-user"
@@ -28,9 +37,12 @@ CREATE DATABASE keycloak
 GRANT ALL PRIVILEGES ON DATABASE postgres TO "kogito-user";
 GRANT ALL PRIVILEGES ON DATABASE kogito TO "kogito-user";
 GRANT ALL PRIVILEGES ON DATABASE kogito TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE xa-object-store TO "kogito-user";
+GRANT ALL PRIVILEGES ON DATABASE xa-object-store TO postgres;
 
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO "kogito-user";
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO postgres;
+
 
 CREATE ROLE "grafana-user" WITH
     LOGIN
