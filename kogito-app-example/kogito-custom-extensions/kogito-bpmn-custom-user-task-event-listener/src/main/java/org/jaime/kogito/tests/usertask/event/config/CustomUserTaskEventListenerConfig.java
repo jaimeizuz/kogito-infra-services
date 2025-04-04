d1b5bc42e6748f2039ead4c18a6fcdf1a0878f7b
Jaime Izuzquiza Suárez-Inclán <jizuzquiza@hotmail.es>
2025-04-04 12:09:58 +0200

    Fix UserTaskEventListener overriding default one

--- kogito-app-example/kogito-custom-extensions/kogito-bpmn-custom-user-task-event-listener/src/main/java/org/jaime/kogito/tests/usertask/event/config/CustomUserTaskEventListenerConfig.java
+++ kogito-app-example/kogito-custom-extensions/kogito-bpmn-custom-user-task-event-listener/src/main/java/org/jaime/kogito/tests/usertask/event/config/CustomUserTaskEventListenerConfig.java
@@ -4,17 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.jaime.kogito.tests.usertask.event.impl.CustomUserTaskEventListener;
+import org.kie.kogito.uow.events.UnitOfWorkUserTaskEventListener;
 import org.kie.kogito.usertask.UserTaskEventListener;
 import org.kie.kogito.usertask.UserTaskEventListenerConfig;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
-@ApplicationScoped
-public class CustomUserTaskEventListenerConfig implements UserTaskEventListenerConfig {
+//@ApplicationScoped
+public class CustomUserTaskEventListenerConfig {
+// implements UserTaskEventListenerConfig {
 
+/*
     @Override
     public List<UserTaskEventListener> listeners() {
-        return Arrays.asList(new CustomUserTaskEventListener());
+        return Arrays.asList(new CustomUserTaskEventListener(), new UnitOfWorkUserTaskEventListener);
     }
+*/
     
 }
